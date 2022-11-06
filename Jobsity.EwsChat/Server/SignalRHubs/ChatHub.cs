4921f8b5fa04ed256cc964115309f2544415e45c
ewerton@getskimmer.com <ewerton@getskimmer.com>
2022-11-06 15:06:43 -0300

    SignalR hub

---
+++ Jobsity.EwsChat/Server/SignalRHubs/ChatHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Jobsity.EwsChat.Server.SignalRHubs
+{
+    //[Authorize]?
+    public class ChatHub : Hub
+    {
+        private static readonly Dictionary<string, string> Users = new();
+
+        public override async Task OnConnectedAsync()
+        {
+            string user = Context.GetHttpContext()?.Request.Query["user"] ?? "New user";
+
+            if (user != null && !Users.ContainsKey(user))
+            {
+                Users.Add(Context.ConnectionId, user);
+            }
+
+            await AddMessage(string.Empty, $"{user} joined the chat.");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var user = Users.FirstOrDefault(userEntry => userEntry.Key == Context.ConnectionId).Value;
+            await AddMessage(string.Empty, $"{user} has left the chat.");
+            Users.Remove(user);
+
+        }
+
+        public async Task AddMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
