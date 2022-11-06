e3526e6ffe7181bb2a6467dc4ef4bb662d10392f
ewerton@getskimmer.com <ewerton@getskimmer.com>
2022-11-06 15:08:44 -0300

    Stock dto

--- Jobsity.EwsChat/Server/DTO/StockDto.cs
+++ Jobsity.EwsChat/Server/DTO/StockDto.cs
@@ -3,8 +3,15 @@
     public class StockDto
     {
         public string Symbol { get; set; } = string.Empty;
-        public decimal Open { get; set; }
-        public decimal Low { get; set; }
-        public decimal Close { get; set; }
+        public string Open { get; set; } = string.Empty;
+        public string Low { get; set; } = string.Empty;
+        public string Close { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return Close != "N/D" 
+                ? $"{Symbol.ToUpperInvariant()} quote is ${Close} per share." 
+                : "Sorry the information about this stock is unavailable in the Stooq API.";
+        }
     }
 }
