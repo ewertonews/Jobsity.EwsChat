06dfef986429551f861fb7995bb0d770d342779d
ewerton@getskimmer.com <ewerton@getskimmer.com>
2022-11-06 15:10:22 -0300

    Serilog

---
+++ Jobsity.EwsChat/Shared/ILoggingService.cs
@@ -0,0 +1,12 @@
+﻿using Serilog;
+
+namespace Jobsity.EwsChat.Shared
+{
+    public interface ILoggingService
+    {
+        void LogDebug(string message);
+        void LogError(string message, Exception exception);
+        void LogInfo(string message);
+        void LogWarn(string message);
+    }
+}
---
+++ Jobsity.EwsChat/Shared/LoggingService.cs
@@ -0,0 +1,12 @@
+﻿using Serilog;
+
+namespace Jobsity.EwsChat.Shared
+{
+    public class LoggingService : ILoggingService
+    {
+        public void LogDebug(string message) => Log.Debug(message);
+        public void LogError(string message, Exception exception) => Log.Error(message, exception);
+        public void LogInfo(string message) => Log.Information(message);
+        public void LogWarn(string message) => Log.Warning(message);
+    }
+}
