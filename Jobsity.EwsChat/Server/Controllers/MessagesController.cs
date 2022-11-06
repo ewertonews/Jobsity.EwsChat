e8a861400ba3bd595f2a061565404896a27b09f6
ewerton@getskimmer.com <ewerton@getskimmer.com>
2022-11-06 15:10:36 -0300

    Messages controller

--- Jobsity.EwsChat/Server/Controllers/MessagesController.cs
+++ Jobsity.EwsChat/Server/Controllers/MessagesController.cs
@@ -1,6 +1,4 @@
-﻿using System.Net.Mime;
-using Jobsity.EwsChat.Server.Services;
-using Jobsity.EwsChat.Shared;
+﻿using Jobsity.EwsChat.Server.Services;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,42 +9,20 @@ namespace Jobsity.EwsChat.Server.Controllers
     [ApiController]
     public class MessagesController : ControllerBase
     {
-        private readonly IMessagesService _messagesService;
+        private readonly IChatBotService _chatBotService;
 
-        public MessagesController(IMessagesService messagesService)
+        public MessagesController(IChatBotService chatBotService)
         {
-            _messagesService = messagesService;
+            _chatBotService = chatBotService;
         }
 
         [HttpGet]
         [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
-        public async Task<ActionResult<string>> Get([FromQuery] string roomId)
+        public async Task<ActionResult<bool>> Get(string message)
         {
-            var stringMessages = await _messagesService.GetMessages(roomId);
-            if (string.IsNullOrEmpty(stringMessages))
-            {
-                return NotFound();
-            }
-
-            return Ok(stringMessages);
-        }
-
-        [HttpPost]
-        [Consumes(MediaTypeNames.Application.Json)]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
-        public async Task<ActionResult<bool>> Post(Message message)
-        {
-            var addMessageSucceeded = await _messagesService.AddMessage(message);
-            if (!addMessageSucceeded)
-            {
-                return BadRequest();
-            }
-
-            //normally it would be: CreatedAtAction(nameof(GetById), new { id = entity.Id }, entity);
-            //but it's not needed in this context.
-            return new ObjectResult(true) { StatusCode = StatusCodes.Status201Created };
+            
+            await _chatBotService.ProcessSpecialMessage(message);
+            return Ok(true);
         }
 
     }
